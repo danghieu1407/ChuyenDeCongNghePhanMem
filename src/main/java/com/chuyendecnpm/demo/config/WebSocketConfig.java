5eab37013ebe5f45a64b2319b33146c00b7edb50
danghieu1407 <danghieu140701@gmail.com>
2022-05-23 00:30:01 +0700

    delete note

--- src/main/java/com/chuyendecnpm/demo/config/WebSocketConfig.java
+++ src/main/java/com/chuyendecnpm/demo/config/WebSocketConfig.java
@@ -17,8 +17,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");   // Enables a simple in-memory broker
-
+        registry.enableSimpleBroker("/topic"); 
 
     
     }
